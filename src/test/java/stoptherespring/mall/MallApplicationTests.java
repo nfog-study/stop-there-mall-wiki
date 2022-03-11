c2f98de8586ca9705244782af29ac8a87d8e8be1
황정훈[PaaS Dev2] <junghun.hwang@navercorp.com>
2022-03-11 12:08:36 +0900

    init setting

---
+++ src/test/java/stoptherespring/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package stoptherespring.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
